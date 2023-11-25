a1e32a1fc46be3cddc86376c051464c5c8c208ee
SC <>
2023-11-24 17:00:19 -0800

    fishing anadia gives sanity

---
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/thebetweenlands/MixinItemMobAnadia.java
@@ -0,0 +1,37 @@
+package com.seriouscreeper.bladditions.mixins.modsupport.thebetweenlands;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.tiffit.sanity.Sanity;
+import net.tiffit.sanity.SanityCapability;
+import net.tiffit.sanity.SanityModifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import thebetweenlands.common.item.misc.ItemMobAnadia;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Mixin(value = ItemMobAnadia.class)
+public class MixinItemMobAnadia {
+    @Inject(method = "onCapturedByPlayer", at = @At("HEAD"))
+    private void injectOnCapturedByPlayer(EntityPlayer player, EnumHand hand, ItemStack stack, EntityLivingBase entity, CallbackInfo ci) {
+        if (!player.world.isRemote) {
+            SanityCapability cap = (SanityCapability)player.getCapability(SanityCapability.INSTANCE, (EnumFacing)null);
+            List<SanityModifier> mods = Sanity.getModifierValues("misc");
+            Iterator var3 = mods.iterator();
+
+            while(var3.hasNext()) {
+                SanityModifier mod = (SanityModifier)var3.next();
+                if (mod.value.equals("fish")) {
+                    cap.increaseSanity(mod.amount);
+                }
+            }
+        }
+    }
+}

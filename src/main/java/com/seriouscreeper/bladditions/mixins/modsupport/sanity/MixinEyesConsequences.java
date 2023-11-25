493f1d11932b9b94b4ddc10550b4fd1fc671de84
SC <>
2023-11-24 22:04:23 -0800

    fixed too many eyes spawning

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/sanity/MixinEyesConsequences.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/sanity/MixinEyesConsequences.java
@@ -1,7 +1,9 @@
 package com.seriouscreeper.bladditions.mixins.modsupport.sanity;
 
+import gigaherz.eyes.ConfigData;
 import gigaherz.eyes.entity.EntityEyes;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.tiffit.sanity.SanityCapability;
 import net.tiffit.sanity.consequences.EyesConsequence;
@@ -9,9 +11,17 @@ import net.tiffit.sanity.proxy.CommonProxy;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import thebetweenlands.common.entity.mobs.EntityAnadia;
+
+import java.util.List;
 
 @Mixin(value = EyesConsequence.class, remap = false)
 public class MixinEyesConsequences {
+    @Unique
+    private final int into_the_betweenlands_mod$radius = 32;
+
+
     /**
      * @author SC
      */
@@ -21,10 +31,18 @@ public class MixinEyesConsequences {
         if (pos != null) {
             SanityCapability cap = player.getCapability(SanityCapability.INSTANCE, null);
 
-            if(cap != null && cap.getSanityExact() < 0) {
+            if(cap != null && cap.getSanityExact() <= -20) {
                 float chance = cap.getSanityExact() / -200F;
 
                 if(player.getRNG().nextFloat() < chance) {
+                    AxisAlignedBB aabb = (new AxisAlignedBB(player.getPosition())).grow(into_the_betweenlands_mod$radius);
+
+                    List<EntityEyes> otherEyes = player.getServerWorld().getEntitiesWithinAABB(EntityEyes.class, aabb, (a) -> a.getDistanceSq((double)((float)player.getPosition().getX() + 0.5F), (double)((float)player.getPosition().getY() + 0.5F), (double)((float)player.getPosition().getZ() + 0.5F)) <= (double)((float) into_the_betweenlands_mod$radius * (float) into_the_betweenlands_mod$radius));
+
+                    if(otherEyes.size() >= ConfigData.MaximumPackSize) {
+                        return;
+                    }
+
                     EntityEyes eyes = new EntityEyes(player.world);
                     eyes.setPosition(pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5);
                     player.world.spawnEntity(eyes);

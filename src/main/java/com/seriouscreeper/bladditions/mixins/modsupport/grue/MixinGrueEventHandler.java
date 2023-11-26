c3af04645cb97623199a540c7544ffa5d32a765d
SC <>
2023-11-25 19:47:06 -0800

    remap grue class

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/grue/MixinGrueEventHandler.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/grue/MixinGrueEventHandler.java
@@ -28,13 +28,10 @@ import net.tiffit.sanity.SanityCapability;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Arrays;
 
-@Mixin(value = GrueEventHandler.class)
+@Mixin(value = GrueEventHandler.class, remap = false)
 public class MixinGrueEventHandler {
     /**
      * @author

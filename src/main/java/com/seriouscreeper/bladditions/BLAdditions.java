78a64c38e34c447569b1ad231ac02579dd5df228
SC <>
2023-11-30 01:57:23 -0800

    upped version to 1.4

--- src/main/java/com/seriouscreeper/bladditions/BLAdditions.java
+++ src/main/java/com/seriouscreeper/bladditions/BLAdditions.java
@@ -49,7 +49,7 @@ public class BLAdditions
 {
     public static final String MODID = "bladditions";
     public static final String NAME = "BL Additions";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
 
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;

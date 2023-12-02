456f9962e7eae597f11fe1994f66a1e2d516fe25
SC <>
2023-12-02 13:27:40 -0800

    have every tea give fundamental research

--- src/main/java/com/seriouscreeper/bladditions/events/BLAdditionsEventHandler.java
+++ src/main/java/com/seriouscreeper/bladditions/events/BLAdditionsEventHandler.java
@@ -1127,6 +1127,11 @@ public class BLAdditionsEventHandler {
                         int oProg = IPlayerKnowledge.EnumKnowledgeType.OBSERVATION.getProgression();
                         int tProg = IPlayerKnowledge.EnumKnowledgeType.THEORY.getProgression();
 
+                        if(world.rand.nextInt(100) <= 17) {
+                            ThaumcraftApi.internalMethods.addKnowledge(player, IPlayerKnowledge.EnumKnowledgeType.OBSERVATION, ResearchCategories.getResearchCategory("BASICS"), MathHelper.getInt(player.getRNG(), oProg / 2, oProg));
+                            ThaumcraftApi.internalMethods.addKnowledge(player, IPlayerKnowledge.EnumKnowledgeType.THEORY, ResearchCategories.getResearchCategory("BASICS"), MathHelper.getInt(player.getRNG(), tProg / 3, tProg / 2));
+                        }
+
                         switch(researchPotion.Category) {
                             case ARCANE:
                                 ThaumcraftApi.internalMethods.addKnowledge(player, IPlayerKnowledge.EnumKnowledgeType.OBSERVATION, ResearchCategories.getResearchCategory("INFUSION"), MathHelper.getInt(player.getRNG(), oProg / 2, oProg));

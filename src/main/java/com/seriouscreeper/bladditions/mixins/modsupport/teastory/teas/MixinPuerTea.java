abea86ad18f00d0aa4a8f87fce5d143dc326be8e
SC <>
2023-12-04 00:39:31 -0800

    changed teas so they all have the same buff time and strength

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/teastory/teas/MixinPuerTea.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/teastory/teas/MixinPuerTea.java
@@ -2,18 +2,52 @@ package com.seriouscreeper.bladditions.mixins.modsupport.teastory.teas;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import roito.teastory.api.drink.DailyDrink;
+import roito.teastory.config.ConfigMain;
+import roito.teastory.helper.DrinkingHelper;
+import roito.teastory.item.ItemRegister;
 import roito.teastory.item.drink.PuerTea;
 
 @Mixin(value = PuerTea.class, remap = false)
 public class MixinPuerTea {
-    @Shadow
+    /**
+     * @author SC
+     * @reason
+     */
+    @Overwrite
     public static void addPotion(int tier, World world, EntityPlayer entityplayer) {
+        if (!world.isRemote) {
+            DailyDrink dailyDrink = DrinkingHelper.getLevelAndTimeImprovement(world, entityplayer);
+            tier += dailyDrink.getLevel();
+            int time = ConfigMain.drink.puerTeaDrink_Time;
+            time = (int)((float)time * (1.0F + dailyDrink.getTime()));
+            int addedTimePercent = (int)(dailyDrink.getTime() * 100.0F);
+            entityplayer.sendMessage(new TextComponentTranslation("teastory.message.daily_drinking.normal", new Object[]{dailyDrink.getInstantDay(), addedTimePercent + "%", dailyDrink.getLevel()}));
+            if (ConfigMain.general.useTeaResidueAsBoneMeal) {
+                ItemHandlerHelper.giveItemToPlayer(entityplayer, new ItemStack(ItemRegister.tea_residue, 1, 1));
+            }
+
+            String[] var6 = ConfigMain.drink.puerTeaDrink_Effect;
+            int var7 = var6.length;
+
+            for(int var8 = 0; var8 < var7; ++var8) {
+                String potion = var6[var8];
+                if (Potion.getPotionFromResourceLocation(potion) != null) {
+                    entityplayer.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation(potion), time / (tier + 1), tier));
+                }
+            }
+        }
     }
 
     @Inject(method = "onFoodEaten", at = @At("HEAD"), cancellable = true)
--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/teastory/teas/MixinYellowTea.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/teastory/teas/MixinYellowTea.java
@@ -2,23 +2,59 @@ package com.seriouscreeper.bladditions.mixins.modsupport.teastory.teas;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import roito.teastory.api.drink.DailyDrink;
+import roito.teastory.config.ConfigMain;
+import roito.teastory.helper.DrinkingHelper;
+import roito.teastory.item.ItemRegister;
 import roito.teastory.item.drink.YellowTea;
+import roito.teastory.potion.PotionRegister;
 
 @Mixin(value = YellowTea.class, remap = false)
 public class MixinYellowTea {
-    @Shadow
-    public static void addPotion(int tier, World world, EntityPlayer entityplayer) {
-    }
-
     @Inject(method = "onFoodEaten", at = @At("HEAD"), cancellable = true)
     private void injectOnFoodEaten(ItemStack itemstack, World world, EntityPlayer entityplayer, CallbackInfo ci) {
         addPotion(0, world, entityplayer);
         ci.cancel();
+    }
+
+
+    /**
+     * @author SC
+     * @reason
+     */
+    @Overwrite
+    public static void addPotion(int tier, World world, EntityPlayer entityplayer) {
+        if (!world.isRemote) {
+            DailyDrink dailyDrink = DrinkingHelper.getLevelAndTimeImprovement(world, entityplayer);
+            tier += dailyDrink.getLevel();
+            int time = ConfigMain.drink.yellowTeaDrink_Time;
+            time = (int)((float)time * (1.0F + dailyDrink.getTime()));
+            int addedTimePercent = (int)(dailyDrink.getTime() * 100.0F);
+            entityplayer.sendMessage(new TextComponentTranslation("teastory.message.daily_drinking.normal", new Object[]{dailyDrink.getInstantDay(), addedTimePercent + "%", dailyDrink.getLevel()}));
+            if (ConfigMain.general.useTeaResidueAsBoneMeal) {
+                ItemHandlerHelper.giveItemToPlayer(entityplayer, new ItemStack(ItemRegister.tea_residue, 1, 1));
+            }
+
+            String[] var6 = ConfigMain.drink.yellowTeaDrink_Effect;
+            int var7 = var6.length;
+
+            for(int var8 = 0; var8 < var7; ++var8) {
+                String potion = var6[var8];
+                if (Potion.getPotionFromResourceLocation(potion) != null) {
+                    entityplayer.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation(potion), time / (tier + 1), tier));
+                }
+            }
+        }
     }
 }

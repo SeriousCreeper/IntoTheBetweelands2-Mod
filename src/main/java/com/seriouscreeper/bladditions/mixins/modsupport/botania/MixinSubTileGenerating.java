33acf11e6f60719913335eb9007f8c170a4b16ea
SC <>
2024-02-02 20:22:47 -0800

    made flowers decay into dead weedwood bushes

---
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/botania/MixinSubTileGenerating.java
@@ -0,0 +1,16 @@
+package com.seriouscreeper.bladditions.mixins.modsupport.botania;
+
+import net.minecraft.block.state.IBlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import thebetweenlands.common.registries.BlockRegistry;
+import vazkii.botania.api.subtile.SubTileGenerating;
+
+@Mixin(value = SubTileGenerating.class, remap = false)
+public class MixinSubTileGenerating {
+    @ModifyArg(method = "onUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;)Z"), index = 1)
+    private IBlockState modifyDeadBush(IBlockState state) {
+        return BlockRegistry.DEAD_WEEDWOOD_BUSH.getDefaultState();
+    }
+}

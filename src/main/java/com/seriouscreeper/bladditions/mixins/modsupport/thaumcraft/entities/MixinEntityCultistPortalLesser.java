53bf06969b0a2e1b87024469bed90aea5ae64fd8
SC <>
2024-02-02 23:23:15 -0800

    crimson portals can no longer be healed

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/thaumcraft/entities/MixinEntityCultistPortalLesser.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/thaumcraft/entities/MixinEntityCultistPortalLesser.java
@@ -1,9 +1,18 @@
 package com.seriouscreeper.bladditions.mixins.modsupport.thaumcraft.entities;
 
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import thaumcraft.common.entities.monster.cult.EntityCultistPortalLesser;
 import thebetweenlands.api.entity.IEntityBL;
 
 @Mixin(value = EntityCultistPortalLesser.class, remap = false)
-public class MixinEntityCultistPortalLesser implements IEntityBL {
+public class MixinEntityCultistPortalLesser extends EntityMob implements IEntityBL {
+    public MixinEntityCultistPortalLesser(World worldIn) {
+        super(worldIn);
+    }
+
+    @Override
+    public void heal(float healAmount) {
+    }
 }

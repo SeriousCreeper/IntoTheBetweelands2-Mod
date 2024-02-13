0bce89eb88833ff636b61a85ee66ffa6345169a0
SC <>
2024-02-12 22:28:57 -0800

    reduce arcane archive tank size

---
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/arcanearchives/MixinRadiantTankTileEntity.java
@@ -0,0 +1,17 @@
+package com.seriouscreeper.bladditions.mixins.modsupport.arcanearchives;
+
+import com.aranaira.arcanearchives.tileentities.RadiantTankTileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = RadiantTankTileEntity.class, remap = false)
+public class MixinRadiantTankTileEntity {
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public int getCapacity(int capacity) {
+        return 10000 + ((capacity + 1) * 2000);
+    }
+}

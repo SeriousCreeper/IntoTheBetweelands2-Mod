61027ab40e7a83467c772ec3fd577adfb4d2f53e
SC <>
2024-02-14 23:29:47 -0800

    remove block rotation from return portal

---
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/arcaneworld/MixinBlockReturnPortal.java
@@ -0,0 +1,23 @@
+package com.seriouscreeper.bladditions.mixins.modsupport.arcaneworld;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import party.lemons.arcaneworld.block.BlockReturnPortal;
+
+@Mixin(value = BlockReturnPortal.class, remap = false)
+public class MixinBlockReturnPortal {
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return false;
+    }
+}

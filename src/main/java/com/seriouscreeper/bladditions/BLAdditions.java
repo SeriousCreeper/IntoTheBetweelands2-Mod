756a843eea8afc9330ed3d9fc1323fc5553dd397
SC <>
2024-02-17 18:15:51 -0800

    upped version

--- src/main/java/com/seriouscreeper/bladditions/BLAdditions.java
+++ src/main/java/com/seriouscreeper/bladditions/BLAdditions.java
@@ -51,7 +51,7 @@ public class BLAdditions
 {
     public static final String MODID = "bladditions";
     public static final String NAME = "BL Additions";
-    public static final String VERSION = "1.5.4";
+    public static final String VERSION = "1.5.5";
 
     public static final ResourceLocation ANCIENT_ARMOR_CHEST = registerLootTable("loot/ancient_armor_chest");
 

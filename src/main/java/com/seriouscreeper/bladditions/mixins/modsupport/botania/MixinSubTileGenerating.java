d74c38831661f2db707203a668e988d8fa989963
SC <>
2024-02-20 19:05:16 -0800

    remove remap from flower

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/botania/MixinSubTileGenerating.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/botania/MixinSubTileGenerating.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import thebetweenlands.common.registries.BlockRegistry;
 import vazkii.botania.api.subtile.SubTileGenerating;
 
-@Mixin(value = SubTileGenerating.class, remap = false)
+@Mixin(value = SubTileGenerating.class)
 public class MixinSubTileGenerating {
     @ModifyArg(method = "onUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;)Z"), index = 1)
     private IBlockState modifyDeadBush(IBlockState state) {

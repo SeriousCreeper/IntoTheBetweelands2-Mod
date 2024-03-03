ee3f6f1fc173ade23b5aafc79a04146d9b5ed6a7
SC <>
2024-03-02 16:24:58 -0800

    fixed bound shovel not digging up dirt

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/bloodmagic/MixinItemBoundShovel.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/bloodmagic/MixinItemBoundShovel.java
@@ -10,6 +10,7 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multimap;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
@@ -18,23 +19,27 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Enchantments;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import thebetweenlands.api.item.CorrosionHelper;
 import thebetweenlands.api.item.ICorrodible;
+import thebetweenlands.common.entity.mobs.EntityTinySludgeWorm;
+import thebetweenlands.common.registries.AdvancementCriterionRegistry;
+import thebetweenlands.common.registries.BlockRegistry;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Set;
@@ -75,6 +80,79 @@ public class MixinItemBoundShovel extends ItemBoundTool implements IActivatable,
     public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
         return CorrosionHelper.shouldCauseReequipAnimation(oldStack, newStack, slotChanged);
     }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Nonnull
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+
+        if(!this.getActivated(player.getHeldItem(hand))) {
+            return EnumActionResult.PASS;
+        }
+
+        if (facing == EnumFacing.UP) {
+            boolean dug = false;
+            IBlockState blockState = world.getBlockState(pos);
+            if (blockState.getBlock() == BlockRegistry.COARSE_SWAMP_DIRT) {
+                world.setBlockState(pos, BlockRegistry.DUG_SWAMP_DIRT.getDefaultState());
+                dug = true;
+            }
+
+            if (blockState.getBlock() == BlockRegistry.SWAMP_DIRT) {
+                world.setBlockState(pos, BlockRegistry.DUG_SWAMP_DIRT.getDefaultState());
+                dug = true;
+                this.checkForWormSpawn(world, pos, player);
+            }
+
+            if (blockState.getBlock() == BlockRegistry.SWAMP_GRASS) {
+                world.setBlockState(pos, BlockRegistry.DUG_SWAMP_GRASS.getDefaultState());
+                dug = true;
+                this.checkForWormSpawn(world, pos, player);
+            }
+
+            if (blockState.getBlock() == BlockRegistry.PURIFIED_SWAMP_DIRT) {
+                world.setBlockState(pos, BlockRegistry.DUG_PURIFIED_SWAMP_DIRT.getDefaultState());
+                dug = true;
+            }
+
+            if (dug) {
+                if (world.isRemote) {
+                    for(int i = 0; i < 80; ++i) {
+                        world.spawnParticle(EnumParticleTypes.BLOCK_CRACK, (double)((float)pos.getX() + 0.5F), (double)(pos.getY() + 1), (double)((float)pos.getZ() + 0.5F), (double)((world.rand.nextFloat() - 0.5F) * 0.1F), (double)(world.rand.nextFloat() * 0.3F), (double)((world.rand.nextFloat() - 0.5F) * 0.1F), new int[]{Block.getStateId(blockState)});
+                    }
+                }
+
+                SoundType sound = blockState.getBlock().getSoundType(blockState, world, pos, player);
+
+                for(int i = 0; i < 3; ++i) {
+                    world.playSound((EntityPlayer)null, (double)((float)pos.getX() + hitX), (double)((float)pos.getY() + hitY), (double)((float)pos.getZ() + hitZ), sound.getBreakSound(), SoundCategory.PLAYERS, 1.0F, 0.5F + world.rand.nextFloat() * 0.5F);
+                }
+
+                player.getHeldItem(hand).damageItem(1, player);
+                return EnumActionResult.SUCCESS;
+            }
+        }
+
+        return EnumActionResult.PASS;
+    }
+
+
+    @Unique
+    public void checkForWormSpawn(World world, BlockPos pos, EntityPlayer player) {
+        if (!world.isRemote && world.getDifficulty() != EnumDifficulty.PEACEFUL && world.rand.nextInt(12) == 0) {
+            EntityTinySludgeWorm entity = new EntityTinySludgeWorm(world);
+            entity.setLocationAndAngles((double)pos.getX() + 0.5D, (double)pos.getY() + 1.0D, (double)pos.getZ() + 0.5D, 0.0F, 0.0F);
+            world.spawnEntity(entity);
+            if (player instanceof EntityPlayerMP) {
+                AdvancementCriterionRegistry.WORM_FROM_DIRT.trigger((EntityPlayerMP)player);
+            }
+        }
+    }
+
 
     /**
      * @author

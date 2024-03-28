5328d7fdf26d274a5465349a1b02d5e35fe69b01
SC <>
2024-03-27 20:44:12 -0700

    made naturally spawned pyrads and infestations ignore pacifists

---
+++ src/main/java/com/seriouscreeper/bladditions/mixins/MixinEntityAINearestAttackableTarget.java
@@ -0,0 +1,48 @@
+package com.seriouscreeper.bladditions.mixins;
+
+import com.seriouscreeper.bladditions.proxy.CommonProxy;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAITarget;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import thebetweenlands.common.entity.mobs.EntityPyrad;
+import thebetweenlands.common.entity.mobs.EntitySwarm;
+
+@Mixin(value = EntityAINearestAttackableTarget.class)
+public class MixinEntityAINearestAttackableTarget<T extends EntityLivingBase> extends EntityAITarget {
+    @Shadow
+    protected T targetEntity;
+
+    public MixinEntityAINearestAttackableTarget(EntityCreature creature, boolean checkSight) {
+        super(creature, checkSight);
+    }
+
+    @Inject(method = "startExecuting", at = @At("HEAD"), cancellable = true)
+    private void avoidPacifist(CallbackInfo ci) {
+        if(this.taskOwner instanceof EntitySwarm ||
+           (this.taskOwner instanceof EntityPyrad && this.taskOwner.getEntityAttribute(EntityPyrad.AGRESSIVE).getAttributeValue() != 1))
+        {
+            if(this.targetEntity instanceof EntityPlayer && !this.targetEntity.world.isRemote) {
+                EntityPlayerMP player = (EntityPlayerMP) this.targetEntity;
+
+                if(!CommonProxy.IsPacifist(player)) {
+                    return;
+                }
+
+                ci.cancel();
+            }
+        }
+    }
+
+    @Shadow
+    public boolean shouldExecute() {
+        return false;
+    }
+}

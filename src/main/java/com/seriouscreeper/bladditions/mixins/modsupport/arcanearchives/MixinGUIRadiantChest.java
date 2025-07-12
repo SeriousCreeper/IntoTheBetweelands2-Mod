a2fa0eb005daab0eb6fa0f2548ba233439af6c88
SC <>
2025-07-12 11:05:43 -0700

    fixed arcane chest hotfix not working in pack

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/arcanearchives/MixinGUIRadiantChest.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/arcanearchives/MixinGUIRadiantChest.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = GUIRadiantChest.class, remap = false)
+@Mixin(value = GUIRadiantChest.class)
 public class MixinGUIRadiantChest {
     @Shadow private Slot hoveredSlot;
 

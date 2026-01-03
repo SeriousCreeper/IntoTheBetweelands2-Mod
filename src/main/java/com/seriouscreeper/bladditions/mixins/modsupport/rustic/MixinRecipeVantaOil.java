831bf7a34d32a8d234bd8f7d042017b22f75b685
SeriousCreeper <seriouscreeper@gmail.com>
2026-01-03 12:53:58 -0800

    removed some vanta oil recipes

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/rustic/MixinRecipeVantaOil.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/rustic/MixinRecipeVantaOil.java
@@ -1,4 +1,26 @@
 package com.seriouscreeper.bladditions.mixins.modsupport.rustic;
 
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import rustic.common.crafting.RecipeVantaOil;
+
+@Mixin(value = RecipeVantaOil.class)
 public class MixinRecipeVantaOil {
+    @Overwrite
+    public boolean matches(InventoryCrafting inv, World worldIn) {
+        return false;
+    }
+
+    @Overwrite
+    public ItemStack getCraftingResult(InventoryCrafting inv) {
+        return ItemStack.EMPTY;
+    }
+
+    @Overwrite
+    public boolean isDynamic() {
+        return false;
+    }
 }
--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/rustic/MixinRusticUtils.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/rustic/MixinRusticUtils.java
@@ -1,4 +1,14 @@
 package com.seriouscreeper.bladditions.mixins.modsupport.rustic;
 
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import rustic.common.util.RusticUtils;
+
+@Mixin(value = RusticUtils.class)
 public class MixinRusticUtils {
+    @Overwrite
+    public static boolean isVantaOilableWeapon(ItemStack stack) {
+        return false;
+    }
 }

a8751b22386db8773d66686ba4216d48fecbe50e
SeriousCreeper <seriouscreeper@gmail.com>
2026-01-05 23:26:22 -0800

    fixed IE machine block triggers

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/immersiveengineering/MixinMultiblockArcFurnace.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/immersiveengineering/MixinMultiblockArcFurnace.java
@@ -13,6 +13,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -100,5 +101,10 @@ public class MixinMultiblockArcFurnace {
                 cir.cancel();
             }
         }
+    }
+
+    @Overwrite
+    public boolean isBlockTrigger(IBlockState state) {
+        return state.getBlock() == BlocksTC.crucible;
     }
 }

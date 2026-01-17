3bbdaa0dd7184135b006f1427ad65832fb6150c9
SeriousCreeper <seriouscreeper@gmail.com>
2026-01-16 23:19:22 -0800

    fixed radiant trove max fluid count

--- src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/arcanearchives/MixinRadiantTankTileEntity.java
+++ src/main/java/com/seriouscreeper/bladditions/mixins/modsupport/arcanearchives/MixinRadiantTankTileEntity.java
@@ -1,11 +1,21 @@
 package com.seriouscreeper.bladditions.mixins.modsupport.arcanearchives;
 
 import com.aranaira.arcanearchives.tileentities.RadiantTankTileEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = RadiantTankTileEntity.class, remap = false)
 public class MixinRadiantTankTileEntity {
+    @Shadow
+    @Final
+    private RadiantTankTileEntity.VoidingFluidTank inventory;
+
     /**
      * @author
      * @reason
@@ -13,5 +23,25 @@ public class MixinRadiantTankTileEntity {
     @Overwrite
     public int getCapacity(int capacity) {
         return 10000 + (capacity * 2000);
+    }
+
+    @ModifyConstant(
+            method = "<init>",
+            constant = @Constant(intValue = 16000)
+    )
+    private static int changeBaseCapacity(int original) {
+        return 10000;
+    }
+
+    @ModifyArg(
+            method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/aranaira/arcanearchives/tileentities/RadiantTankTileEntity$VoidingFluidTank;<init>(Lcom/aranaira/arcanearchives/tileentities/RadiantTankTileEntity;I)V"
+            ),
+            index = 1
+    )
+    private int changeTankCapacity(int original) {
+        return 10000;
     }
 }
